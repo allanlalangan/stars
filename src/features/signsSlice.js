988b83416bc2bbf5eb5b8dfdac2ce20063124e4a
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-09-20 18:08:01 -0700

    init signsSlice and actions

--- src/features/signsSlice.js
+++ src/features/signsSlice.js
@@ -1,26 +1,31 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  aries: { users: 0 },
-  taurus: { users: 0 },
-  gemini: { users: 0 },
-  cancer: { users: 0 },
-  leo: { users: 0 },
-  virgo: { users: 0 },
-  libra: { users: 0 },
-  scorpio: { users: 0 },
-  sagittarius: { users: 0 },
-  capricorn: { users: 0 },
-  aquarius: { users: 0 },
-  pisces: { users: 0 },
+  signs: [
+    { name: 'aries', users: 0 },
+    { name: 'taurus', users: 0 },
+    { name: 'gemini', users: 0 },
+    { name: 'cancer', users: 0 },
+    { name: 'leo', users: 0 },
+    { name: 'virgo', users: 0 },
+    { name: 'libra', users: 0 },
+    { name: 'scorpio', users: 0 },
+    { name: 'sagittarius', users: 0 },
+    { name: 'capricorn', users: 0 },
+    { name: 'aquarius', users: 0 },
+    { name: 'pisces', users: 0 },
+  ],
 };
-
 const signsSlice = createSlice({
   name: 'signs',
   initialState,
   reducers: {
-    increment(state) {},
-    decrement(state) {},
+    decrement: (state, action) => {
+      state.signs.find((sign) => sign.name === action.payload).users -= 1;
+    },
+    increment: (state, action) => {
+      state.signs.find((sign) => sign.name === action.payload).users += 1;
+    },
   },
 });
 
