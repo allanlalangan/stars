43191109796a5516d3e505a33800422bc15e751b
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-09-20 18:08:58 -0700

    init store Provider, create dummy data and Counter component

---
+++ public/dummy.js
@@ -0,0 +1,16 @@
+const signs = [
+  'aries',
+  'taurus',
+  'gemini',
+  'cancer',
+  'leo',
+  'virgo',
+  'libra',
+  'scorpio',
+  'sagittarius',
+  'capricorn',
+  'aquarius',
+  'pisces',
+];
+
+export default signs;
--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Counter from './components/Counter';
+
 const App = () => {
   return (
     <div>
       <h1 className='text-red-400 font-medium'>Stars</h1>
+      <Counter />
     </div>
   );
 };
