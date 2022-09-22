7720162b3a6ea2ab1ddb061aded88e075bfdf937
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-09-22 10:10:38 -0700

    add links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 
 import HomePage from './pages/home';
 import GenerateChartPage from './pages/generate-chart';
-
-// import Counter from './components/Counter';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <h1 className='font-medium text-red-400'>Stars</h1>
-        <main>
+        <header className='flex w-full justify-between bg-sky-50 px-4'>
+          <Link to='/'>
+            <h1 className='cursor-pointer font-medium text-red-400'>Stars</h1>
+          </Link>
+          <nav className='flex items-center'>
+            <Link to='/generate-chart'>
+              <button>Generate Natal Chart</button>
+            </Link>
+          </nav>
+        </header>
+        <main className='px-4'>
           <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/generate-chart' element={<GenerateChartPage />} />
           </Routes>
         </main>
       </BrowserRouter>
-      {/* <NatalForm /> */}
     </div>
   );
 };
