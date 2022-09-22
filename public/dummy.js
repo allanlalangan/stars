f38290baa6927f773eefac612515fbeacc218f82
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-09-22 00:17:05 -0700

    add months data

--- public/dummy.js
+++ public/dummy.js
@@ -1,4 +1,4 @@
-const signs = [
+export const signs = [
   'aries',
   'taurus',
   'gemini',
@@ -13,4 +13,17 @@ const signs = [
   'pisces',
 ];
 
-export default signs;
+export const months = [
+  { name: 'January', days: 31 },
+  { name: 'February', days: 28 },
+  { name: 'March', days: 31 },
+  { name: 'April', days: 30 },
+  { name: 'May', days: 31 },
+  { name: 'June', days: 30 },
+  { name: 'July', days: 31 },
+  { name: 'August', days: 31 },
+  { name: 'September', days: 30 },
+  { name: 'October', days: 31 },
+  { name: 'November', days: 30 },
+  { name: 'December', days: 31 },
+];
