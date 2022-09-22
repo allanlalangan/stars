d7201cd8b3d1b3e99887fabe0c711948ee95c6f1
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-09-22 09:43:37 -0700

    cleanup api

--- src/api/natalAPI.js
+++ src/api/natalAPI.js
@@ -1,24 +1,30 @@
 import axios from 'axios';
 
+const configBase = {
+  method: 'POST',
+  url: `https://json.astrologyapi.com/v1/`,
+  headers: {
+    authorization:
+      'Basic ' +
+      btoa(
+        process.env.REACT_APP_ASTROLOGY_API_USER_ID +
+          ':' +
+          process.env.REACT_APP_ASTROLOGY_API_KEY
+      ),
+    'Content-Type': 'application/json',
+  },
+};
+
 export const getGeo = async (value, source) => {
   try {
     const config = {
+      ...configBase,
       cancelToken: source.token,
       method: 'POST',
       url: `https://json.astrologyapi.com/v1/geo_details`,
       data: {
         place: value,
         maxRows: 5,
-      },
-      headers: {
-        authorization:
-          'Basic ' +
-          btoa(
-            process.env.REACT_APP_ASTROLOGY_API_USER_ID +
-              ':' +
-              process.env.REACT_APP_ASTROLOGY_API_KEY
-          ),
-        'Content-Type': 'application/json',
       },
     };
 
@@ -33,21 +39,12 @@ export const getGeo = async (value, source) => {
 export const getTimeZone = async (value) => {
   try {
     const config = {
+      ...configBase,
       method: 'POST',
       url: `https://json.astrologyapi.com/v1/timezone`,
       data: {
         country_code: value,
         isDst: true,
-      },
-      headers: {
-        authorization:
-          'Basic ' +
-          btoa(
-            process.env.REACT_APP_ASTROLOGY_API_USER_ID +
-              ':' +
-              process.env.REACT_APP_ASTROLOGY_API_KEY
-          ),
-        'Content-Type': 'application/json',
       },
     };
 
