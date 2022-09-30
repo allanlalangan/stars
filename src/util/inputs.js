757fe082b6be5d18408e5697648acbd3b2f04bef
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-09-29 19:19:59 -0700

    add inputs util props

---
+++ src/util/inputs.js
@@ -0,0 +1,61 @@
+export const inputs = [
+  {
+    name: 'username',
+    type: 'text',
+    placeholder: 'Username',
+    label: 'Username',
+    required: true,
+    errorMessage: '',
+  },
+  {
+    name: 'email',
+    type: 'email',
+    placeholder: 'Email',
+    label: 'Email',
+    required: true,
+    errorMessage: '',
+  },
+  {
+    name: 'password',
+    type: 'password',
+    placeholder: 'Password',
+    label: 'Password',
+    required: true,
+    errorMessage:
+      'Password should contain 8-20 characters. 1 lowercase letter, 1 uppercase letter, 1 number, and 1 special character required.',
+    pattern:
+      '(?=[A-Za-z0-9@#$%^&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+!=])(?=.{8,20}).*$',
+  },
+  {
+    name: 'confirm',
+    type: 'password',
+    placeholder: 'Confirm Password',
+    label: 'Confirm Password',
+    required: true,
+    errorMessage: '',
+  },
+  {
+    name: 'birthdate',
+    type: 'date',
+    placeholder: '',
+    label: '',
+    required: false,
+    errorMessage: '',
+  },
+  {
+    name: 'birthtime',
+    type: 'time',
+    placeholder: '',
+    label: '',
+    required: false,
+    errorMessage: '',
+  },
+  {
+    name: 'profilePic',
+    type: 'text',
+    placeholder: 'Upload Picture',
+    label: 'Profile Picture',
+    required: false,
+    errorMessage: '',
+  },
+];
