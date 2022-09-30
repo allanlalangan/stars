b0aed76e20244af036f5c76530a3903d0dc66f92
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-09-29 18:11:10 -0700

    add dashboard tiles

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -5,7 +5,26 @@ const DashboardPage = () => {
     <>
       <section className='flex h-full'>
         <Sidebar />
-        <h2 className='m-auto'>Dashboard</h2>
+        <article className='w-full overflow-scroll p-4'>
+          <h2 className='m-auto'>Dashboard</h2>
+          <ul className='grid grid-cols-12 gap-4'>
+            <li className='col-span-6 row-span-2 min-h-[20rem] border-2 border-gray-900 p-4'>
+              <h3>Weekly Forecast</h3>
+            </li>
+            <li className='col-span-6 min-h-[20rem] border-2 border-gray-900 p-4'>
+              <h3>The Stars Today</h3>
+            </li>
+            <li className='col-span-6 min-h-[20rem] border-2 border-gray-900 p-4'>
+              <h3>Moon</h3>
+            </li>
+            <li className='col-span-6 min-h-[20rem] border-2 border-gray-900 p-4'>
+              <h3>Upcoming Events</h3>
+            </li>
+            <li className='col-span-6 min-h-[20rem] border-2 border-gray-900 p-4'>
+              <h3>Visualize and Manifest</h3>
+            </li>
+          </ul>
+        </article>
       </section>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/components/*.{js,jsx,ts,tsx}', './src/*.{js,jsx,ts,tsx}'],
+  content: [
+    './src/components/*.{js,jsx,ts,tsx}',
+    './src/pages/*.{js,jsx,ts,tsx}',
+    './src/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     fontFamily: {
       display: ['Inknut Antiqua'],
