ffce77c6d664099955eb9a32de0a9578bfb56ff1
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-10-04 17:24:24 -0700

    add template dashboard components with dum code

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -8,8 +8,97 @@ const DashboardPage = () => {
         <article className='w-full overflow-scroll p-4'>
           <h2 className='mb-4'>Hello, user!</h2>
           <ul className='grid grid-cols-12 gap-4'>
-            <li className='col-span-6 row-span-2 min-h-[20rem] border-2 border-gray-900 p-4'>
+            <li className='col-span-6 row-span-2 flex min-h-[20rem] flex-col border-2 border-gray-900 p-4'>
               <h3>Weekly Forecast</h3>
+              <ul className='grid h-full'>
+                <li className=''>
+                  <h4>Sunday</h4>
+                  <ul>
+                    <li className='flex w-1/4 justify-evenly text-lg'>
+                      <span className='flex w-1/3 items-center justify-center'>
+                        ☉
+                      </span>
+                      <span className='mx-1 flex w-1/3 items-center justify-center'>
+                        →
+                      </span>
+                      <span className='flex w-1/3 items-center justify-center'>
+                        ♑︎
+                      </span>
+                    </li>
+                  </ul>
+                </li>
+                <li className=''>
+                  <h4>Monday</h4>
+                  <ul>
+                    <li className='flex w-1/4 justify-evenly text-lg'>
+                      <span className='flex w-1/3 items-center justify-center'>
+                        ☽
+                      </span>
+                      <span className='mx-1 flex w-1/3 items-center justify-center'>
+                        →
+                      </span>
+                      <span className='flex w-1/3 items-center justify-center'>
+                        ♋︎
+                      </span>
+                    </li>
+                    <li className='flex w-1/4 justify-evenly text-lg'>
+                      <span className='flex w-1/3 items-center justify-center'>
+                        ☉
+                      </span>
+                      <span className='mx-1 flex w-1/3 items-center justify-center'>
+                        ☍
+                      </span>
+                      <span className='flex w-1/3 items-center justify-center'>
+                        ☽
+                      </span>
+                    </li>
+                  </ul>
+                </li>
+                <li className=''>
+                  <h4>Tuesday</h4>
+                  <ul>
+                    <li className='flex w-1/4 justify-evenly text-lg'>
+                      <span className='flex w-1/3 items-center justify-center'>
+                        ♃
+                      </span>
+                      <span className='mx-1 flex w-1/3 items-center justify-center'>
+                        =
+                      </span>
+                      <span className='flex w-1/3 items-center justify-center'>
+                        ℞
+                      </span>
+                    </li>
+                  </ul>
+                </li>
+                <li className=''>
+                  <h4>Wednesday</h4>
+                  <ul>
+                    <li className='flex w-1/4 justify-evenly text-lg'>
+                      <span className='flex w-1/3 items-center justify-center'>
+                        ♄
+                      </span>
+                      <span className='mx-1 flex w-1/3 items-center justify-center'>
+                        ≠
+                      </span>
+                      <span className='flex w-1/3 items-center justify-center'>
+                        ℞
+                      </span>
+                    </li>
+                  </ul>
+                </li>
+                <li className=''>
+                  <h4>Thursday</h4>
+                  <ul></ul>
+                </li>
+                <li className=''>
+                  <h4>Friday</h4>
+                  <ul></ul>
+                </li>
+                <li className=''>
+                  <h4>Saturday</h4>
+                  <ul></ul>
+                </li>
+              </ul>
             </li>
             <li className='col-span-6 flex min-h-[20rem] flex-col border-2 border-gray-900 p-4'>
               <h3 className='h-fit'>The Stars Today</h3>
