40aef507461d87e317040ea9cdd6af4fb363f2d4
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-10-13 10:43:46 -0700

    add nested altar routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import HomePage from './pages/home';
 import LoginPage from './pages/login';
 import CreateAccountPage from './pages/create-account';
 import GenerateChartPage from './pages/generate-chart';
+import ChartsPage from './pages/charts';
+import TarotPage from './pages/tarot';
 import DashboardPage from './pages/dashboard';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
@@ -12,6 +14,7 @@ import Sidebar from './components/Sidebar';
 // import { getCurrentPlanets } from './features/todaySlice';
 import { useSelector } from 'react-redux';
 import AltarPage from './pages/altar';
+import TarotDeckPage from './pages/tarot-deck';
 
 const App = () => {
   const { user } = useSelector((state) => state.auth);
@@ -28,17 +31,16 @@ const App = () => {
           <Sidebar />
           <Routes>
             <Route path='/' element={<HomePage />} />
-            <Route
-              path='/dashboard'
-              element={user ? <DashboardPage /> : <LoginPage />}
-            />
+            <Route path='/dashboard' element={<DashboardPage />} />
             <Route path='/login' element={<LoginPage />} />
             <Route path='/create-account' element={<CreateAccountPage />} />
             <Route path='/generate-chart' element={<GenerateChartPage />} />
-            <Route
-              path='/altar'
-              element={user ? <AltarPage /> : <LoginPage />}
-            />
+            <Route path='/altar' element={<AltarPage />}>
+              <Route path='tarot' element={<TarotPage />}>
+                <Route path='deck/:id' element={<TarotDeckPage />} />
+              </Route>
+              <Route path='charts' element={<ChartsPage />} />
+            </Route>
           </Routes>
         </main>
       </BrowserRouter>
--- client/src/pages/altar.js
+++ client/src/pages/altar.js
@@ -1,7 +1,22 @@
+import { Link, Outlet } from 'react-router-dom';
+
 const AltarPage = () => {
   return (
-    <section className='h-full w-full p-4'>
+    <section className='flex h-full w-full flex-col p-4'>
       <h2>Altar</h2>
+      <nav className='flex w-1/2'>
+        <Link to='tarot'>
+          <button className='mr-2 mt-1 flex cursor-pointer items-center bg-slate-700 p-2 text-lg tracking-wide text-slate-50'>
+            Tarot
+          </button>
+        </Link>
+        <Link to='charts'>
+          <button className='mr-2 mt-1 flex cursor-pointer items-center bg-slate-700 p-2 text-lg tracking-wide text-slate-50'>
+            Charts
+          </button>
+        </Link>
+      </nav>
+      <Outlet />
     </section>
   );
 };
