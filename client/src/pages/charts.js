a0cc5fed11b7841c0dd40cbbda3c8a5b9e60e931
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-10-13 10:43:23 -0700

    add charts and tarot pages

---
+++ client/src/pages/charts.js
@@ -0,0 +1,25 @@
+const ChartsPage = () => {
+  return (
+    <article>
+      <h3>Star Charts</h3>
+      <section className='flex w-full'>
+        <article className='w-2/3'>
+          <h4>Your Chart</h4>
+          <h5>Chart</h5>
+        </article>
+        <aside className='w-1/3'>
+          <ul>
+            <h4>Saved Charts</h4>
+            <li>
+              <h5>Chart</h5>
+            </li>
+            <li>
+              <h5>Chart</h5>
+            </li>
+          </ul>
+        </aside>
+      </section>
+    </article>
+  );
+};
+export default ChartsPage;
---
+++ client/src/pages/tarot.js
@@ -0,0 +1,19 @@
+import { Link, Outlet } from 'react-router-dom';
+
+const TarotPage = () => {
+  return (
+    <article className=''>
+      <h3>Your Tarot Decks</h3>
+      <ul>
+        <li>
+          <h4>Rider Waite</h4>
+        </li>
+
+        <li>
+          <h4>Deck 2</h4>
+        </li>
+      </ul>
+    </article>
+  );
+};
+export default TarotPage;
