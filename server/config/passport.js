01996a4004abe9863a485adbccfccf8c1fd7e4a7
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-10-17 20:59:40 -0700

    npm i passport + oauth20 strategy, sessions create passport config

---
+++ server/config/passport.js
@@ -0,0 +1,26 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/User');
+
+module.exports = function (passport) {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: '/auth/google/callback',
+        scope: ['profile', 'email'],
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        console.log(profile);
+        done(null, profile);
+      }
+    )
+  );
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => done(err, user));
+  });
+};
