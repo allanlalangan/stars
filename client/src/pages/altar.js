0eb821d13e6e08d7be949ea58f2a850a732edd94
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-10-25 12:06:01 -0700

    fix nested charts page with Outlet component

--- client/src/pages/altar.js
+++ client/src/pages/altar.js
@@ -1,21 +1,20 @@
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 const AltarPage = () => {
   return (
     <section className='flex h-full flex-col p-4 md:w-3/4'>
       <h2>Altar</h2>
       <nav className='flex w-1/2'>
-        <Link to='tarot'>
-          <button className='mr-2 mt-1 flex cursor-pointer items-center bg-slate-700 p-2 text-lg tracking-wide text-slate-50'>
-            Divination & Ritual
-          </button>
-        </Link>
+        <button className='mr-2 mt-1 flex cursor-pointer items-center bg-slate-700 p-2 text-lg tracking-wide text-slate-50'>
+          Divination & Ritual
+        </button>
         <Link to='charts'>
           <button className='mr-2 mt-1 flex cursor-pointer items-center bg-slate-700 p-2 text-lg tracking-wide text-slate-50'>
             Charts
           </button>
         </Link>
       </nav>
+      <Outlet />
     </section>
   );
 };
