274d150c8e8382f9d8ba07129f8297d82cd34e53
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-10-26 21:12:37 -0700

    update dashboard styles

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -20,16 +20,27 @@ const DashboardPage = () => {
           <h3 className='bg-slate-800 p-2 font-display text-secondary-200'>
             The Planets Today
           </h3>
-          <article>
-            <ul>
+          <ul className='grid h-full grid-cols-12 gap-2'>
+            {/* <ul>
               {planets?.map((planet, i) => (
                 <li key={i}>
                   {planet.name} at {planet.position.toFixed(2)}º{' '}
                   {getFullSignName(planet.sign)}
                 </li>
               ))}
-            </ul>
-          </article>
+            </ul> */}
+            <li className='col-span-12 border'>Sun</li>
+            <li className='col-span-12 border'>Moon</li>
+            <li className='col-span-4 border'>Mercury</li>
+            <li className='col-span-4 border'>Venus</li>
+            <li className='col-span-4 border'>Mars</li>
+            <li className='col-span-6 border'>Jupiter</li>
+            <li className='col-span-6 border'>Saturn</li>
+            <li className='col-span-4 border'>Uranus</li>
+            <li className='col-span-4 border'>Neptune</li>
+            <li className='col-span-4 border'>Pluto</li>
+            <li className='col-span-12 border'>North Node</li>
+          </ul>
         </section>
         <section className='col-span-6 row-span-1 flex flex-col border-2 border-primary-800 p-4'>
           <h3 className='bg-slate-800 p-2 font-display text-secondary-200'>
@@ -50,10 +61,10 @@ const DashboardPage = () => {
         </section>
         <section className='col-span-6 row-span-2 flex flex-col border-2 border-primary-800 p-4'>
           <h3 className='bg-slate-800 p-2 font-display text-secondary-200'>
-            Upcoming Events
+            For You
           </h3>
           <article className='flex w-1/2 flex-col'>
-            <h4 className='text-[1rem]'>Transits</h4>
+            <h4 className='text-[1rem]'>Recent Entries</h4>
             <div className='mx-2 border-2 border-primary-800 p-2'></div>
           </article>
         </section>
