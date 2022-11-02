3845583672c8ac7697f47d06ac22fad9d3674455
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-11-02 10:23:51 -0700

    create googleOAuth method for User schema

--- server/models/User.js
+++ server/models/User.js
@@ -8,6 +8,7 @@ const UserSchema = Schema(
     firstName: String,
     lastName: String,
     image: String,
+    username: String,
     email: {
       type: String,
       required: [true, 'Please enter an email address'],
@@ -15,7 +16,6 @@ const UserSchema = Schema(
     },
     password: {
       type: String,
-      required: true,
     },
   },
   { timestamps: true }
@@ -29,6 +29,22 @@ UserSchema.statics.registerUser = async function (name, email, password) {
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
   const user = this.create({ firstName: name, email, password: hash });
+  return user;
+};
+
+UserSchema.statics.googleOAuth = async function (decoded) {
+  const existingUser = await this.findOne({ email: decoded.email });
+  if (existingUser) {
+    throw Error('Email is already registered');
+  }
+  const user = this.create({
+    googleId: decoded.sub,
+    firstName: decoded.given_name,
+    lastName: decoded.family_name,
+    email: decoded.email,
+    username: decoded.name,
+    image: decoded.picture,
+  });
   return user;
 };
 
