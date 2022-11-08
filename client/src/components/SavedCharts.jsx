229ee9cb8c9478693a872b662f2f424517a563f9
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-11-08 09:06:48 -0800

    update chart card

--- client/src/components/SavedCharts.jsx
+++ client/src/components/SavedCharts.jsx
@@ -21,17 +21,32 @@ const SavedCharts = () => {
   return (
     <section className='flex w-full flex-col'>
       <h5>Saved Charts</h5>
-      <ul className='flex h-1/2 flex-col sm:h-80 sm:overflow-scroll md:h-96'>
+      <ul className='mr-4 flex h-1/2 flex-col sm:h-80 sm:overflow-scroll md:h-96'>
         {charts?.map((chart) => (
-          <li className='mb-2 cursor-pointer border border-slate-300 py-2 px-4'>
-            <p>{chart.name}</p>
-            <p>{`${getFullSignName(chart.planets.sun.sign)} ${
-              chart.planets.sun.name
-            }`}</p>
-            <p>{`${getFullSignName(chart.planets.moon.sign)} ${
-              chart.planets.moon.name
-            }`}</p>
-            <p>{`${getFullSignName(chart.houses.first_house.sign)} Rising`}</p>
+          <li className='mb-2 flex cursor-pointer items-center justify-between rounded border border-slate-300 py-2 px-4'>
+            <article className='flex w-1/2 flex-col'>
+              <h6 className='font-display'>{chart.name}</h6>
+              <p className='font-heading text-xs'>{`${getFullSignName(
+                chart.planets.sun.sign
+              )} ${chart.planets.sun.name}`}</p>
+              <p className='font-heading text-xs'>{`${getFullSignName(
+                chart.planets.moon.sign
+              )} ${chart.planets.moon.name}`}</p>
+              <p className='font-heading text-xs'>{`${getFullSignName(
+                chart.houses.first_house.sign
+              )} Rising`}</p>
+            </article>
+            <section className='flex w-1/2 flex-col'>
+              <button className='mb-1 rounded border border-primary-700 bg-slate-50 shadow hover:bg-slate-100'>
+                View Full Chart
+              </button>
+              <button className='mb-1 rounded border border-primary-700 bg-slate-50 shadow hover:bg-slate-100'>
+                Set Default
+              </button>
+              <button className='mb-1 rounded border border-primary-700 bg-slate-50 shadow hover:bg-slate-100'>
+                Delete
+              </button>
+            </section>
           </li>
         ))}
       </ul>
