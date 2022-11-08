1f67cca331977e134ffdb79a14b41970f8219bd0
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-11-08 13:29:11 -0800

    add createPost method to Post model

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,14 +1,52 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+const Chart = require('./Chart');
+const User = require('./User');
 const PostSchema = Schema(
   {
     user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    chart: { type: Schema.Types.ObjectId, ref: 'Chart', required: true },
-    body: String,
-    liked_by: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    // chart: { type: Schema.Types.ObjectId, ref: 'Chart', default: null },
+    chart: { type: Object, default: null },
+    body: { type: String, max: 200 },
+    likedBy: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    reaction_LoveBy: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    reaction_FireBy: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    reaction_ApplaudBy: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    reaction_SadBy: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   },
   { timestamps: true }
 );
+
+PostSchema.statics.createPost = async function (req) {
+  let post;
+  if (!req.body.chart) {
+    post = await this.create({
+      user: req.user.id,
+      body: req.body.text,
+    });
+  } else {
+    const chart = await Chart.findById(req.body.chart);
+    const postChart = {
+      name: chart.name,
+      chart_image: chart.chart_image,
+      day: chart.day,
+      month: chart.month,
+      year: chart.year,
+      hour: chart.hour,
+      minute: chart.minute,
+      city: chart.city,
+      planets: chart.planets,
+      houses: chart.houses,
+      lunar_phase: chart.lunar_phase,
+    };
+
+    post = await this.create({
+      user: req.user.id,
+      chart: postChart,
+      body: req.body.text,
+    });
+  }
+  return post;
+};
 
 module.exports = mongoose.model('Post', PostSchema);
