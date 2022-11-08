a33197e22d9e5333d9440c3a29e7f9fa1f4bda28
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-11-08 13:32:12 -0800

    update Post card styles, add delete button

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,83 +1,97 @@
-import { BsThreeDots } from 'react-icons/bs';
+import { MdDeleteForever } from 'react-icons/md';
 
 const Feed = () => {
   return (
-    <section className='flex flex-col overflow-y-scroll p-4'>
-      <article className='mb-4 rounded border-2 border-primary-700 p-4'>
+    <section className='flex flex-col overflow-y-scroll bg-slate-100 p-4'>
+      <article className='mb-4 rounded border border-primary-200 bg-slate-50 p-4 shadow-sm'>
         <section className='flex justify-between'>
           <h4 className='font-display text-lg'>Aquemini420</h4>
-          <button className='border p-2'>
-            <BsThreeDots />
+          <button className='flex items-center rounded border border-primary-400 p-2'>
+            <span className='text-xs font-semibold text-red-400'>
+              Delete Post
+            </span>
+            <MdDeleteForever className='text-lg text-red-400' />
           </button>
         </section>
         <p>celebrating my 33rd journey around the Sun!</p>
         <section className='flex'>
-          <button className='mr-2 mt-2 w-full items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
+          <button className='my-2 w-full items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
             Like
           </button>
         </section>
         <section className='flex'>
-          <button className='mr-2 mt-2 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
+          <button className='mr-1 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
             🔥
           </button>
-          <button className='mr-2 mt-2 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
+          <button className='mx-1 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
             ♥️
           </button>
-          <button className='mr-2 mt-2 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
+          <button className='mx-1 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
             👏
           </button>
-          <button className='mr-2 mt-2 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
+          <button className='ml-1 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
             😢
           </button>
         </section>
       </article>
-      <article className='mb-4 rounded border-2 border-primary-700 p-4'>
+      <article className='mb-4 rounded border border-primary-200 bg-slate-50 p-4 shadow-sm'>
         <section className='flex justify-between'>
           <h4 className='font-display text-lg'>dood69</h4>
-          <button className='border p-2'>
-            <BsThreeDots />
+          <button className='flex items-center rounded border border-primary-400 p-2'>
+            <span className='text-xs font-semibold text-red-400'>
+              Delete Post
+            </span>
+            <MdDeleteForever className='text-lg text-red-400' />
           </button>
         </section>
         <p>Been so tired lately 🥱, just want to stay home</p>
         <section className='flex'>
-          <button className='mr-2 mt-2 w-full items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
+          <button className='my-2 w-full items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
             Like
           </button>
         </section>
         <section className='flex'>
-          <button className='mr-2 mt-2 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
+          <button className='mr-1 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
             🔥
           </button>
-          <button className='mr-2 mt-2 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
+          <button className='mx-1 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
             ♥️
           </button>
-          <button className='mr-2 mt-2 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
+          <button className='mx-1 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
             👏
           </button>
-          <button className='mr-2 mt-2 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
+          <button className='ml-1 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
             😢
           </button>
         </section>
       </article>
-      <article className='mb-4 rounded border-2 border-primary-700 p-4'>
-        <h4 className='font-display text-lg'>touchMyVenus</h4>
+      <article className='mb-4 rounded border border-primary-200 bg-slate-50 p-4 shadow-sm'>
+        <section className='flex justify-between'>
+          <h4 className='font-display text-lg'>touchMyVenus</h4>
+          <button className='flex items-center rounded border border-primary-400 p-2'>
+            <span className='text-xs font-semibold text-red-400'>
+              Delete Post
+            </span>
+            <MdDeleteForever className='text-lg text-red-400' />
+          </button>
+        </section>
         <p>Someone kiss me!😜</p>
         <section className='flex'>
-          <button className='mr-2 mt-2 w-full items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
+          <button className='my-2 w-full items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
             Like
           </button>
         </section>
         <section className='flex'>
-          <button className='mr-2 mt-2 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
+          <button className='mr-1 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
             🔥
           </button>
-          <button className='mr-2 mt-2 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
+          <button className='mx-1 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
             ♥️
           </button>
-          <button className='mr-2 mt-2 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
+          <button className='mx-1 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
             👏
           </button>
-          <button className='mr-2 mt-2 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
+          <button className='ml-1 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
             😢
           </button>
         </section>
