6344f04f68aa09c84f0a1b32f589b6a6682452ea
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-11-08 17:58:55 -0800

    delete dummy

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -4,24 +4,13 @@ import { MdDeleteForever } from 'react-icons/md';
 import { getPosts } from '../features/postsSlice';
 import Post from './Post';
 
-const dummy = [
-  {
-    userDetails: { name: 'Aquemini420' },
-    body: 'celebrating my 33rd journey around the Sun!',
-  },
-  {
-    userDetails: { name: 'dood69' },
-    body: 'Been so tired lately 🥱, just want to stay home',
-  },
-  { userDetails: { name: 'touchMyVenus' }, body: 'Someone kiss me!😜' },
-];
 const Feed = () => {
   const dispatch = useDispatch();
   const { posts } = useSelector((state) => state.posts);
 
   useEffect(() => {
     dispatch(getPosts());
-  }, []);
+  }, [dispatch]);
 
   useEffect(() => {
     console.log(posts);
@@ -30,9 +19,6 @@ const Feed = () => {
     <section className='flex flex-col overflow-y-scroll bg-slate-100 p-4'>
       {posts?.map((post) => (
         <Post key={post._id} post={post} />
-      ))}
-      {dummy?.map((post, i) => (
-        <Post key={i} post={post} />
       ))}
     </section>
   );
