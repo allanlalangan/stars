beb05fa9be1b1afe98ba33d08675a6afe44b6916
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-11-08 16:06:52 -0800

    fix input id bugs update Post reaction buttons

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -19,22 +19,80 @@ const Post = ({ post }) => {
       <p>{post.body}</p>
       {post.chart && <section>{post.chart.name}'s chart</section>}
       <section className='flex'>
-        <button className='my-2 w-full items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
-          Like
+        <button
+          type='button'
+          className='my-2 w-full items-center text-sm tracking-wide'
+        >
+          <input
+            type='checkbox'
+            name='reaction'
+            id={`like ${post._id}`}
+            className='peer hidden'
+          />
+          <label
+            htmlFor={`like ${post._id}`}
+            className='flex cursor-pointer justify-center rounded border border-primary-300 p-2 font-semibold shadow transition hover:border-primary-700 hover:bg-slate-100 hover:text-primary-700 peer-checked:border-transparent peer-checked:bg-primary-700 peer-checked:text-slate-50'
+          >
+            Like
+          </label>
         </button>
       </section>
-      <section className='flex'>
-        <button className='mr-1 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
-          🔥
+      <section className='grid grid-cols-12 gap-2'>
+        <button type='button' className='col-span-3'>
+          <input
+            type='checkbox'
+            name='reaction'
+            id={`fire ${post._id}`}
+            className='peer hidden'
+          />
+          <label
+            htmlFor={`fire ${post._id}`}
+            className='flex cursor-pointer justify-center rounded border border-primary-300 p-2 font-semibold text-primary-400 shadow transition hover:border-primary-700 hover:bg-slate-100 hover:text-primary-700 peer-checked:border-transparent peer-checked:bg-primary-700 peer-checked:text-slate-50'
+          >
+            🔥
+          </label>
         </button>
-        <button className='mx-1 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
-          ♥️
+        <button type='button' className='col-span-3'>
+          <input
+            type='checkbox'
+            name='reaction'
+            id={`pray ${post._id}`}
+            className='peer hidden'
+          />
+          <label
+            htmlFor={`pray ${post._id}`}
+            className='flex cursor-pointer justify-center rounded border border-primary-300 p-2 font-semibold text-primary-400 shadow transition hover:border-primary-700 hover:bg-slate-100 hover:text-primary-700 peer-checked:border-transparent peer-checked:bg-primary-700 peer-checked:text-slate-50'
+          >
+            🙏
+          </label>
         </button>
-        <button className='mx-1 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
-          👏
+        <button type='button' className='col-span-3'>
+          <input
+            type='checkbox'
+            name='reaction'
+            id={`applaud ${post._id}`}
+            className='peer hidden'
+          />
+          <label
+            htmlFor={`applaud ${post._id}`}
+            className='flex cursor-pointer justify-center rounded border border-primary-300 p-2 font-semibold text-primary-400 shadow transition hover:border-primary-700 hover:bg-slate-100 hover:text-primary-700 peer-checked:border-transparent peer-checked:bg-primary-700 peer-checked:text-slate-50'
+          >
+            👏
+          </label>
         </button>
-        <button className='ml-1 w-1/2 items-center rounded border border-primary-400 p-2 text-sm tracking-wide shadow hover:bg-slate-100'>
-          😢
+        <button type='button' className='col-span-3'>
+          <input
+            type='checkbox'
+            name='reaction'
+            id={`sad ${post._id}`}
+            className='peer hidden'
+          />
+          <label
+            htmlFor={`sad ${post._id}`}
+            className='flex cursor-pointer justify-center rounded border border-primary-300 p-2 font-semibold text-primary-400 shadow transition hover:border-primary-700 hover:bg-slate-100 hover:text-primary-700 peer-checked:border-transparent peer-checked:bg-primary-700 peer-checked:text-slate-50'
+          >
+            😢
+          </label>
         </button>
       </section>
     </article>
