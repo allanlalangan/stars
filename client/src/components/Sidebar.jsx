446bd8afaab8843c93ad7caf0f93c694f91bd227
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-11-08 18:49:58 -0800

    update Navbar activeLink styles

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,23 +1,36 @@
 import { MdSpaceDashboard } from 'react-icons/md';
 import { BsStars } from 'react-icons/bs';
-import { FaBookmark } from 'react-icons/fa';
 import { GiCandleLight, GiOpenBook, GiNotebook } from 'react-icons/gi';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
     <nav className='fixed bottom-0 flex h-[10%] w-screen flex-col items-center justify-center bg-slate-700 text-slate-50 md:static md:h-full md:w-1/4 md:justify-between'>
-      <ul className='flex w-full justify-evenly md:flex-col md:justify-start md:p-4'>
-        <Link to='/feed'>
-          <button className='flex w-full cursor-pointer content-between items-center font-heading text-sm font-semibold md:mt-2 md:p-2'>
+      <ul className='flex h-full w-full items-center justify-evenly md:flex-col md:items-start md:justify-start md:p-4'>
+        <NavLink
+          to='/feed'
+          className={({ isActive }) =>
+            `${
+              !isActive ? 'border-transparent' : 'border-secondary-400 shadow'
+            } rounded border p-2 transition md:mb-2 md:w-full`
+          }
+        >
+          <button className='flex w-full cursor-pointer content-between items-center font-heading text-sm font-semibold'>
             <span className='flex w-full items-center transition'>
               <BsStars className='mx-2 text-secondary-200' size='1.25rem' />
               <p className='hidden md:block'>Feed</p>
             </span>
           </button>
-        </Link>
-        <Link to='/dashboard'>
-          <button className='flex w-full cursor-pointer content-between items-center font-heading text-sm font-semibold md:mt-2 md:p-2'>
+        </NavLink>
+        <NavLink
+          to='/dashboard'
+          className={({ isActive }) =>
+            `${
+              !isActive ? 'border-transparent' : 'border-secondary-400'
+            } rounded border p-2 transition md:mb-2 md:w-full`
+          }
+        >
+          <button className='flex w-full cursor-pointer content-between items-center font-heading text-sm font-semibold'>
             <span className='flex w-full items-center transition'>
               <MdSpaceDashboard
                 className='mx-2 text-secondary-200'
@@ -26,9 +39,17 @@ const Sidebar = () => {
               <p className='hidden md:block'>Dashboard</p>
             </span>
           </button>
-        </Link>
-        <Link to='/altar'>
-          <button className='flex w-full cursor-pointer content-between items-center font-heading text-sm font-semibold md:mt-2 md:p-2'>
+        </NavLink>
+
+        <NavLink
+          to='/altar'
+          className={({ isActive }) =>
+            `${
+              !isActive ? 'border-transparent' : 'border-secondary-400'
+            } rounded border p-2 transition md:mb-2 md:w-full`
+          }
+        >
+          <button className='flex w-full cursor-pointer content-between items-center font-heading text-sm font-semibold'>
             <span className='flex w-full items-center transition'>
               <GiCandleLight
                 className='mx-2 text-secondary-200'
@@ -37,31 +58,37 @@ const Sidebar = () => {
               <p className='hidden md:block'>Altar</p>
             </span>
           </button>
-        </Link>
-        <Link to='/journal'>
-          <button className='flex w-full cursor-pointer content-between items-center font-heading text-sm font-semibold md:mt-2 md:p-2'>
+        </NavLink>
+        <NavLink
+          to='/journal'
+          className={({ isActive }) =>
+            `${
+              !isActive ? 'border-transparent' : 'border-secondary-400'
+            } rounded border p-2 transition md:mb-2 md:w-full`
+          }
+        >
+          <button className='flex w-full cursor-pointer content-between items-center font-heading text-sm font-semibold'>
             <span className='flex w-full items-center transition'>
               <GiNotebook className='mx-2 text-secondary-200' size='1.25rem' />
               <p className='hidden md:block'>Journal</p>
             </span>
           </button>
-        </Link>
-        <Link to='/reference'>
-          <button className='flex w-full cursor-pointer content-between items-center font-heading text-sm font-semibold md:mt-2 md:p-2'>
+        </NavLink>
+        <NavLink
+          to='/reference'
+          className={({ isActive }) =>
+            `${
+              !isActive ? 'border-transparent' : 'border-secondary-400'
+            } rounded border p-2 transition md:mb-2 md:w-full`
+          }
+        >
+          <button className='flex w-full cursor-pointer content-between items-center font-heading text-sm font-semibold'>
             <span className='flex w-full items-center transition'>
               <GiOpenBook className='mx-2 text-secondary-200' size='1.25rem' />
               <p className='hidden md:block'>Reference</p>
             </span>
           </button>
-        </Link>
-        {/* <Link to='/'>
-          <button className='flex w-full cursor-pointer content-between items-center font-heading text-sm font-semibold md:mt-2 md:p-2'>
-            <span className='flex w-full items-center transition'>
-              <FaBookmark className='mx-2 text-secondary-200' size='1.25rem' />
-              <p className='hidden md:block'>Archive</p>
-            </span>
-          </button>
-        </Link> */}
+        </NavLink>
       </ul>
     </nav>
   );
